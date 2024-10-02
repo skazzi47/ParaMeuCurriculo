58b0381f575122179c855a522348bb34b82c970d
Gabriel Araujo <gabrielaraujomonteiro12@gmail.com>
2024-10-02 08:47:09 -0300

    AtualizacaoREADME

---
+++ JogoDeAdivnhacao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("|\tAdivinhe O Número!\t|\n");
+        Console.WriteLine("Você deve adivinhar o número de 1 à 100:\n");
+        Random random = new Random();
+        int numeroAleatorio = random.Next(1, 100);
+        Console.WriteLine("Número Gerado! Agora tente adivinhar qual número foi gerado:\n");
+        return1:
+        if(int.TryParse(Console.ReadLine(),out int palpite));
+        {
+            if(palpite == numeroAleatorio)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Você ACERTOU!\n");
+                Console.ResetColor();
+            }
+            else if(palpite < numeroAleatorio)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\nO número gerado é maior!\n");
+                Console.ResetColor();
+                goto return1;
+            }
+            else if (palpite > numeroAleatorio)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\nO número gerado é menor!\n");
+                Console.ResetColor();
+                goto return1;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ParaMeuCurriculo
+Atividade desenvolvida em sala de aula.
+---
+### Atividade Escolhida: Jogo de Adivinhação de Números.
+Desenvlvido por: Guilherme Mauricio de Oliveira Damasceno
+Descrição: O código gera um número aleatório e o usuário deve a divinhar qual número foi gerado.
+
+### O que foi utilizado:
+Biblioteca ``System``, Comando ``new Random`` e ``random.next``, Estruturas de Comparação ``if`` e ``else if``.
+
+### Etapas Implementadas:
+- [x] Apresentar ao usuário o nome do jogo.
+- [x] Informa-lo que um número de 1 à 100 foi gerado aleatoriamente.
+- [x] Se o número informado pelo usuário for MENOR que o número gerado, informa o usuário que o número aleatorio é MAIOR.
+- [x] Se o número informado pelo usuário for MAIOR que o número gerado, informa o usuário que o número aleatorio é MENOR.
+- [x] Se o número informado pelo usuário for IGUAL ao número gerado, informa o usuário que ele ACERTOU.
+- [x] Fim do Programa.
+
+### Backlog:
+-[] Realizar um jogo onde o usuário deve adivinhar o número gerado pelo programa.
+
+### Conclusão:
+Esse programa faz a função de gerar um número aleatório para que o usuário possa o adivinhar, uma forma de entretenimento e aprendizado integrados.
